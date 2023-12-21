b556580b9babd05c67501869c23241ef597f2a06
Joaquin Rapela <joacorapela@gmail.com>
2023-12-21 18:52:57 +0000

    Adapted to lds restructure and changed default params

--- code/scripts/doPlotSmoothedTrajectory.py
+++ code/scripts/doPlotSmoothedTrajectory.py
@@ -5,20 +5,26 @@ import datetime
 import argparse
 import plotly.graph_objects as go
 
+sys.path.append("../../../../lds_python/src")
+import lds.tracking.plotting
+
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--first_sample", type=int, default=0,
+    parser.add_argument("--first_sample", type=int, default=122000,
                         help="start position to smooth")
-    parser.add_argument("--number_samples", type=int, default=325910,
+    parser.add_argument("--number_samples", type=int, default=200000,
                         help="number of samples to smooth")
-    parser.add_argument("--first_sample_plot", type=int, default=50000,
+    parser.add_argument("--first_sample_plot", type=int, default=0,
                         help="start position to plot")
-    parser.add_argument("--number_samples_plot", type=int, default=10000,
+    parser.add_argument("--number_samples_plot", type=int, default=100000,
                         help="number of samples to plot")
-    parser.add_argument("--sample_rate", type=float, default=1,
+    parser.add_argument("--sample_rate", type=float, default=40,
                         help="sample rate")
+    parser.add_argument("--min_like", type=float, default=0.2,
+                        help=("x and y of samples with likelihood lower than "
+                              "min_like is set to nan"))
     parser.add_argument("--variable", type=str, default="pos",
-                        help="variable to plot: pos, vel, acc")
+                        help="variable to plot: pos2D, pos, vel, acc")
     parser.add_argument("--color_measured", type=str, default="black",
                         help="color for measured markers")
     parser.add_argument("--color_filtered", type=str, default="red",
@@ -30,10 +36,10 @@ def main(argv):
     parser.add_argument("--symbol_y", type=str, default="circle-open",
                         help="color for y markers")
     parser.add_argument("--smoothed_data_filename_pattern", type=str,
-                        default="../../results/smoothed_results_firstSample{:d}_numberOfSamples{:d}.csv",
+                        default="../../results/smoothed_results_firstSample{:d}_numberOfSamples{:d}_minLike{:.02f}.csv",
                         help="smoothed data filename pattern")
     parser.add_argument("--fig_filename_pattern", type=str,
-                        default="../../figures/smoothed_results_{:s}_firstSample{:d}_numberOfSamples{:d}_plotFromSample{:d}To{:d}.{:s}",
+                        default="../../figures/smoothed_results_{:s}_firstSample{:d}_numberOfSamples{:d}_minLike{:.02f}_plotFromSample{:d}To{:d}.{:s}",
                         help="figure filename pattern")
 
     args = parser.parse_args()
@@ -43,6 +49,7 @@ def main(argv):
     first_sample_plot = args.first_sample_plot
     number_samples_plot = args.number_samples_plot
     sample_rate = args.sample_rate
+    min_like = args.min_like
     variable = args.variable
     color_measured = args.color_measured
     color_filtered = args.color_filtered
@@ -53,7 +60,7 @@ def main(argv):
     fig_filename_pattern = args.fig_filename_pattern
 
     smoothed_data_filename = args.smoothed_data_filename_pattern.format(
-        first_sample, number_samples)
+        first_sample, number_samples, min_like)
     smoothed_data = pd.read_csv(smoothed_data_filename)
     y = np.transpose(smoothed_data[["pos1", "pos2"]].to_numpy())
     dt = 1.0/sample_rate
@@ -70,162 +77,59 @@ def main(argv):
     y_acc_fd[:, 1:] = (y_vel_fd[:, 1:] - y_vel_fd[:, :-1])/dt
     y_acc_fd[:, 0] = y_acc_fd[:, 1]
 
-    fig = go.Figure()
-    if variable == "pos":
-        trace_fd = go.Scatter(x=y[0, :], y=y[1, :],
-                              mode="markers",
-                              marker={"color": color_measured},
-                              customdata=smoothed_data["time"],
-                              hovertemplate="<b>x:</b>%{x:.3f}<br><b>y</b>:%{y:.3f}<br><b>time</b>:%{customdata} sec",
-                              name="measured",
-                              showlegend=True,
-                              )
-        trace_filtered = go.Scatter(x=smoothed_data["fpos1"],
-                                    y=smoothed_data["fpos2"],
-                                    mode="markers",
-                                    marker={"color": color_filtered},
-                                    customdata=smoothed_data["time"],
-                                    hovertemplate="<b>x:</b>%{x:.3f}<br><b>y</b>:%{y:.3f}<br><b>time</b>:%{customdata} sec",
-                                    name="filtered",
-                                    showlegend=True,
-                                    )
-        trace_smoothed = go.Scatter(x=smoothed_data["spos1"],
-                                    y=smoothed_data["spos2"],
-                                    mode="markers",
-                                    marker={"color": color_smoothed},
-                                    customdata=smoothed_data["time"],
-                                    hovertemplate="<b>x:</b>%{x:.3f}<br><b>y</b>:%{y:.3f}<br><b>time</b>:%{customdata} sec",
-                                    name="smoothed",
-                                    showlegend=True,
-                                    )
-        fig.add_trace(trace_fd)
-        fig.add_trace(trace_filtered)
-        fig.add_trace(trace_smoothed)
-        fig.update_layout(xaxis_title="x (pixels)", yaxis_title="y (pixels)",
-                          paper_bgcolor='rgba(0,0,0,0)',
-                          plot_bgcolor='rgba(0,0,0,0)')
+    if variable == "pos2D":
+        fig = lds.tracking.plotting.get_fig_mfs_positions_2D(
+            time=smoothed_data["time"],
+            measured_x=y[0, :],
+            measured_y=y[1, :],
+            filtered_x=smoothed_data["fpos1"],
+            filtered_y=smoothed_data["fpos2"],
+            smoothed_x=smoothed_data["spos1"],
+            smoothed_y=smoothed_data["spos2"],
+        )
+    elif variable == "pos":
+        fig = lds.tracking.plotting.get_fig_mfdfs_kinematics_1D(
+            time=smoothed_data["time"],
+            yaxis_title="position (pixels)",
+            measured_x=y[0, :],
+            measured_y=y[1, :],
+            filtered_x=smoothed_data["fpos1"],
+            filtered_y=smoothed_data["fpos2"],
+            smoothed_x=smoothed_data["spos1"],
+            smoothed_y=smoothed_data["spos2"],
+        )
     elif variable == "vel":
-        trace_fd_x = go.Scatter(x=smoothed_data["time"],
-                                 y=y_vel_fd[0, :],
-                                 mode="lines+markers",
-                                 marker={"color": color_measured},
-                                 name="finite diff x",
-                                 showlegend=True,
-                                 )
-        trace_fd_y = go.Scatter(x=smoothed_data["time"],
-                                 y=y_vel_fd[1, :],
-                                 mode="lines+markers",
-                                 marker={"color": color_measured},
-                                 name="finite diff y",
-                                 showlegend=True,
-                                 )
-        trace_filtered_x = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["fvel1"],
-                                      mode="lines+markers",
-                                      marker={"color": color_filtered,
-                                              "symbol": symbol_x},
-                                      name="filtered x",
-                                      showlegend=True,
-                                      )
-        trace_filtered_y = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["fvel2"],
-                                      mode="lines+markers",
-                                      marker={"color": color_filtered,
-                                              "symbol": symbol_y},
-                                      name="filtered y",
-                                      showlegend=True,
-                                      )
-        trace_smoothed_x = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["svel1"],
-                                      mode="lines+markers",
-                                      marker={"color": color_smoothed,
-                                              "symbol": symbol_x},
-                                      name="smoothed x",
-                                      showlegend=True,
-                                      )
-        trace_smoothed_y = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["svel2"],
-                                      mode="lines+markers",
-                                      marker={"color": color_smoothed,
-                                              "symbol": symbol_y},
-                                      name="smoothed y",
-                                      showlegend=True,
-                                      )
-        fig.add_trace(trace_fd_x)
-        fig.add_trace(trace_fd_y)
-        fig.add_trace(trace_filtered_x)
-        fig.add_trace(trace_filtered_y)
-        fig.add_trace(trace_smoothed_x)
-        fig.add_trace(trace_smoothed_y)
-        fig.update_layout(xaxis_title="time", yaxis_title="velocity",
-                          paper_bgcolor='rgba(0,0,0,0)',
-                          plot_bgcolor='rgba(0,0,0,0)')
+        fig = lds.tracking.plotting.get_fig_mfdfs_kinematics_1D(
+            time=smoothed_data["time"],
+            yaxis_title="velocity (pixels/sec)",
+            fd_x=y_vel_fd[0, :],
+            fd_y=y_vel_fd[1, :],
+            filtered_x=smoothed_data["fvel1"],
+            filtered_y=smoothed_data["fvel2"],
+            smoothed_x=smoothed_data["svel1"],
+            smoothed_y=smoothed_data["svel2"],
+        )
     elif variable == "acc":
-        trace_fd_x = go.Scatter(x=smoothed_data["time"],
-                                 y=y_acc_fd[0, :],
-                                 mode="lines+markers",
-                                 marker={"color": color_measured},
-                                 name="finite diff x",
-                                 showlegend=True,
-                                 )
-        trace_fd_y = go.Scatter(x=smoothed_data["time"],
-                                 y=y_acc_fd[1, :],
-                                 mode="lines+markers",
-                                 marker={"color": color_measured},
-                                 name="finite diff y",
-                                 showlegend=True,
-                                 )
-        trace_filtered_x = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["facc1"],
-                                      mode="lines+markers",
-                                      marker={"color": color_filtered,
-                                              "symbol": symbol_x},
-                                      name="filtered x",
-                                      showlegend=True,
-                                      )
-        trace_filtered_y = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["facc2"],
-                                      mode="lines+markers",
-                                      marker={"color": color_filtered,
-                                              "symbol": symbol_y},
-                                      name="filtered y",
-                                      showlegend=True,
-                                      )
-        trace_smoothed_x = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["sacc1"],
-                                      mode="lines+markers",
-                                      marker={"color": color_smoothed,
-                                              "symbol": symbol_x},
-                                      name="smoothed x",
-                                      showlegend=True,
-                                      )
-        trace_smoothed_y = go.Scatter(x=smoothed_data["time"],
-                                      y=smoothed_data["sacc2"],
-                                      mode="lines+markers",
-                                      marker={"color": color_smoothed,
-                                              "symbol": symbol_y},
-                                      name="smoothed y",
-                                      showlegend=True,
-                                      )
-        fig.add_trace(trace_fd_x)
-        fig.add_trace(trace_fd_y)
-        fig.add_trace(trace_filtered_x)
-        fig.add_trace(trace_filtered_y)
-        fig.add_trace(trace_smoothed_x)
-        fig.add_trace(trace_smoothed_y)
-        fig.update_layout(xaxis_title="time", yaxis_title="acceleration",
-                          paper_bgcolor='rgba(0,0,0,0)',
-                          plot_bgcolor='rgba(0,0,0,0)')
+        fig = lds.tracking.plotting.get_fig_mfdfs_kinematics_1D(
+            time=smoothed_data["time"],
+            yaxis_title="acceleration pixels/sec^2",
+            fd_x=y_acc_fd[0, :],
+            fd_y=y_acc_fd[1, :],
+            filtered_x=smoothed_data["facc1"],
+            filtered_y=smoothed_data["facc2"],
+            smoothed_x=smoothed_data["sacc1"],
+            smoothed_y=smoothed_data["sacc2"],
+        )
     else:
-        raise ValueError("variable={:s} is invalid. It should be: pos, vel, acc".format(variable))
+        raise ValueError("variable={:s} is invalid. It should be: pos2D, pos, vel, acc".format(variable))
 
     fig.write_image(fig_filename_pattern.format(variable, first_sample,
-                                                number_samples,
+                                                number_samples, min_like,
                                                 first_sample_plot,
                                                 number_samples_plot,
                                                 "png"))
     fig.write_html(fig_filename_pattern.format(variable, first_sample,
-                                               number_samples,
+                                               number_samples, min_like,
                                                 first_sample_plot,
                                                 number_samples_plot,
                                                "html"))
